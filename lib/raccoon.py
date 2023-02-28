74e3a8fce8ae1fbeaf469353c2d47997aed7b1e5
brewchetta <chett.tiller@gmail.com>
2023-02-28 13:43:44 -0500

    removed code for lecture

--- lib/raccoon.py
+++ lib/raccoon.py
@@ -11,47 +11,16 @@ class Raccoon:
         return f'<Raccoon id={self.id} name={self.name} trashcan={self.trashcan} >'
 
     @classmethod
-    def create_table(self):
-        sql = """
-            CREATE TABLE IF NOT EXISTS raccoons (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                trashcan TEXT
-            )
-        """
-
-        CURSOR.execute(sql)
+    def create_table(cls):
+        pass
 
     @classmethod
     def all(cls):
-        sql = """
-            SELECT * FROM raccoons
-        """
-
-        raccoons = CURSOR.execute(sql).fetchall()
-        all = []
-        for row in raccoons:
-            raccoon = cls(row[1], row[2])
-            raccoon.id = row[0]
-            all.append(raccoon)
-
-        return all
+        pass
 
     @classmethod
     def find_by_id(cls, id):
-        sql = """
-            SELECT * FROM raccoons WHERE id = ?
-        """
-
-        row = CURSOR.execute(sql, id).fetchone()
-        return row
+        pass
 
     def save(self):
-        sql = """
-            INSERT INTO raccoons (name, trashcan) VALUES (?, ?)
-        """
-
-        CURSOR.execute(sql, (self.name, self.trashcan))
-        CONN.commit()
-
-        self.id = CURSOR.execute("SELECT * FROM raccoons DESC LIMIT 1").fetchone()[0]
+        pass
